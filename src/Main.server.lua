c2b47ed570af12c388d900e87ba2bdf4025cb966
Ozzypig <ozzypig@gmail.com>
2022-04-11 12:58:25 -0400

    Update pj-portal-roblox-web-api-sdk to v1.1.2

--- src/Main.server.lua
+++ src/Main.server.lua
@@ -1,12 +1,11 @@
 local PopJamConnect = require(script.Parent:WaitForChild("PopJamConnect"))
 
--- During the event setup process, RukkazAPI calls this with the event ID
--- that is currently being set up. The function should return the place ID
--- to which event guests must be teleported. The place ID is then passed to
--- TeleportService:ReserveServer
-local function getHostPlaceIdCallback(_eventId)
+-- During the event setup process, the PopJamConnect module invokes this
+-- with the event ID that is currently being set up. The function should
+-- return the place ID to which event guests must be teleported.
+-- The place ID is passed to TeleportService:ReserveServer
+local function getStartPlaceIdCallback(_eventId)
 	return game.PlaceId
 end
-PopJamConnect.RukkazAPI:setEventHostPlaceIdCallback(getHostPlaceIdCallback)
-
+PopJamConnect:setStartPlaceIdCallback(getStartPlaceIdCallback)
 PopJamConnect:main()
